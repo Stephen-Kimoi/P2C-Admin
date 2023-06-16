f1593f34760edefc157ac2313c160b2f509e7470
Stephen Kimoi <moenisteve@gmail.com>
2023-06-16 10:44:25 +0300

    configuring hardhat file and deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
-  defaultNetwork: "polygon_mumbai",
+  defaultNetwork: "sepolia",
   networks: {
     hardhat: {}, 
     polygon_mumbai: {
